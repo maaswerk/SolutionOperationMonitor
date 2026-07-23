6ec517d7832ac846f0535e9a3860b8a45587cd87
Fabian Maas <maastyx@gmx.net>
2026-07-23 21:31:24 +0200

    Update new Version

--- src/AssemblyInfo.cs
+++ src/AssemblyInfo.cs
@@ -8,5 +8,5 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyCopyright("Copyright © 2026")]
 [assembly: ComVisible(false)]
 [assembly: Guid("7f3d2a91-4c6e-4b8a-9d1f-2e5a8c7b6d40")]
-[assembly: AssemblyVersion("1.1.1.0")]
-[assembly: AssemblyFileVersion("1.1.1.0")]
+[assembly: AssemblyVersion("1.3.0.0")]
+[assembly: AssemblyFileVersion("1.3.0.0")]
